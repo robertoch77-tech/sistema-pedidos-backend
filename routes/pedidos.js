6ec1798af4223d4e56b8268dcd15843852582d56
ROBERTO <robertoch.77@gmail.com>
2026-06-15 10:01:23 -0300

    Fix duplicado express en pedidos.js

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db');
-const express = require('express');
-const router = express.Router();
-const pool = require('../db');
 const { Pool } = require('pg');
 
 const conexiones = {};
